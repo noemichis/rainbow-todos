815629494e4807da06f35c533e86fab7834fe307
noemichis <noemi_chis@yahoo.com>
2023-01-11 21:56:00 +0000

    tidy up comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,6 @@ function time() {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     const date = new Date();
-    const year = date.getFullYear();
     const month = months[date.getMonth()];
     const day = date.getDate();
     const weekday = weekdays[date.getDay()];
